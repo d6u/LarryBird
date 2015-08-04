907d6a88978dea87af983e137bd17b2930805804
Daiwei <daiweilu123@gmail.com>
2015-08-04 08:46:09 -0400

    Add helpers to assist web login

--- LarryBird/request.swift
+++ LarryBird/request.swift
@@ -1,9 +1,11 @@
 import Foundation
 import Alamofire
 
+public typealias FinishClosure = (error: NSError?, data: [String: AnyObject]?) -> Void
+
 public func request
     (config: Config)
-    (_ endpoint: Endpoint, params: [Param], finish: (error: NSError?, data: [String: AnyObject]?) -> Void)
+    (_ endpoint: Endpoint, _ params: [Param], _ finish: FinishClosure)
 {
     Alamofire.request(buildRequest(config, endpoint, params))
         .validate()
@@ -14,4 +16,29 @@ public func request
                 finish(error: error, data: nil)
             }
         }
+}
+
+public func requestWebAuthUrl(config: Config)(_ callbackUrl: String, finish: (error: NSError?, url: NSURL?) -> Void) {
+    request(config)(.OauthRequestToken, [.OauthCallback(callbackUrl)]) { error, data in
+        let token = data?["oauth_token"] as? String
+        let url = NSURL(string: "https://api.twitter.com/oauth/authenticate?oauth_token=\(token)")
+        finish(error: error, url: url)
+    }
+}
+
+public func requestAccessToken(config: Config)(_ oauthReturnUrl: NSURL, finish: FinishClosure) {
+    let dict = parseQueryParams(oauthReturnUrl.query!)
+
+    if let verifier = dict["oauth_verifier"] where dict["oauth_token"] != nil {
+        request(config)(.OauthAccessToken, [.OauthVerifier(verifier)], finish)
+    } else {
+        let error = NSError(
+            domain: "com.daiweilu.LarryBird",
+            code: 0,
+            userInfo: [
+                "description": "OAuth return does not contain any verifier",
+                "provided_url": oauthReturnUrl.URLString
+            ])
+        finish(error: error, data: nil)
+    }
 }
